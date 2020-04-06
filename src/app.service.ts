a7e3fd7ff50fcd43b3827d8e7e33efe004c14db5
Yash Murty <yashmurty@gmail.com>
2020-04-07 01:09:04 +0900

    :construction: Add serverless YAML file

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'NestJS 7. Serverless Framework. Azure Functions.';
   }
 }
