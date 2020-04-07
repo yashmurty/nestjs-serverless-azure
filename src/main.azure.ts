3229ba0a60d16cc64063dbcd27f58bd60170bc78
Yash Murty <yashmurty@gmail.com>
2020-04-07 19:05:23 +0900

    :construction: Set AzureHTTP Adapter

--- src/main.azure.ts
+++ src/main.azure.ts
@@ -1,9 +1,10 @@
 import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AzureHttpRouter } from '@nestjs/azure-func-http';
 
 export async function createApp(): Promise<INestApplication> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new AzureHttpRouter());
   app.setGlobalPrefix('api/v1');
   
   await app.init();
